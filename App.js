65894b9347368b7233b4ca322a17b983a5dcc213
Michael Luque <cabezon04.ml@gmail.com>
2022-01-14 15:48:08 -0500

    cambio de color fondo y fuente

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{color: '#888', fontSize: 18}}>
+      Presiona aqui para compartir la foto!
+      </Text>
     </View>
   );
 }
