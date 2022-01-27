69f1ff645c403e26d8de10a7b0ad29949510a49c
anthony luque <kasuru@hotmail.com>
2022-01-27 18:48:40 -0500

    [ADD] icon to eyes, nose and mouth and its logic

--- src/face_detection/FaceDetection.js
+++ src/face_detection/FaceDetection.js
@@ -10,6 +10,16 @@ function FaceDetection() {
   const [type, setType] = useState("back");
   const [eyeLeft, setEyeLeft] = useState({ x: 0, y: 0 });
   const [eyeRigth, setEyeRigth] = useState({ x: 0, y: 0 });
+  const [rightEyeOpen, setRightEyeOpen] = useState();
+  const [leftEyeOpen, setLeftEyeOpen] = useState();
+  const [bounds, setBounds] = useState();
+  const [rollAngle, setRollAngle] = useState(0);
+  const [rightEar, setRightEar] = useState({ x: 0, y: 0 });
+  const [leftEar, setLeftEar] = useState({ x: 0, y: 0 });
+  const [nose, setNose] = useState({ x: 0, y: 0 });
+  const [mouth, setMouth] = useState({ x: 0, y: 0 });
+  const [smile, setSmile] = useState({ x: 0, y: 0 });
+  const iconSize = 60;
 
   useEffect(() => {
     (async () => {
@@ -25,13 +35,22 @@ function FaceDetection() {
     return <Text>No access to camera</Text>;
   }
   const handleFacesDetected = ({ faces }) => {
-    console.log(faces[0]?.LEFT_EYE);
+    // console.log(faces[0]?.leftEyeOpenProbability);
+    setBounds(faces[0]?.bounds);
+    setLeftEyeOpen(faces[0]?.leftEyeOpenProbability);
+    setRightEyeOpen(faces[0]?.rightEyeOpenProbability);
     setEyeLeft(faces[0]?.LEFT_EYE);
     setEyeRigth(faces[0]?.RIGHT_EYE);
+    setRollAngle(faces[0]?.rollAngle);
+    setRightEar(faces[0]?.RIGHT_EAR);
+    setLeftEar(faces[0]?.LEFT_EAR);
+    setNose(faces[0]?.NOSE_BASE);
+    setMouth(faces[0]?.BOTTOM_MOUTH);
+    setSmile(faces[0]?.smilingProbability);
   };
 
   return (
-    <View style={styles.container}>
+    <View style={styles.cameraContainer}>
       <Camera
         type={type}
         style={{ width: "100%", height: "100%" }}
@@ -52,6 +71,20 @@ function FaceDetection() {
           <Ionicons name="camera-reverse-sharp" size={50} color="pink" />
         </TouchableOpacity>
       </View>
+      {bounds && (
+        <View
+          style={{
+            position: "absolute",
+            top: bounds.origin.y,
+            left: bounds.origin.x,
+            height: bounds.size.height,
+            width: bounds.size.width,
+            borderColor: "red",
+            borderWidth: 2,
+            transform: [{ rotate: `${rollAngle}deg` }],
+          }}
+        />
+      )}
       {eyeLeft && (
         <View
           style={{
@@ -60,9 +93,14 @@ function FaceDetection() {
             left: eyeLeft.x - 30,
             height: 60,
             width: 60,
+            transform: [{ rotate: `${rollAngle}deg` }],
           }}
         >
-          <Ionicons name="eye" size={60} color="red" />
+          {leftEyeOpen >= 0.5 ? (
+            <Ionicons name="eye" size={iconSize} color="white" />
+          ) : (
+            <Ionicons name="eye-off" size={iconSize} color="white" />
+          )}
         </View>
       )}
       {eyeRigth && (
@@ -73,9 +111,83 @@ function FaceDetection() {
             left: eyeRigth.x - 30,
             height: 60,
             width: 60,
+            transform: [{ rotate: `${rollAngle}deg` }],
           }}
         >
-          <Ionicons name="eye" size={60} color="red" />
+          {rightEyeOpen >= 0.5 ? (
+            <Ionicons name="eye" size={iconSize} color="white" />
+          ) : (
+            <Ionicons name="eye-off" size={iconSize} color="white" />
+          )}
+        </View>
+      )}
+      {rightEar && (
+        <View
+          style={{
+            position: "absolute",
+            top: rightEar.y - 30,
+            left: rightEar.x - 60,
+            height: 60,
+            width: 60,
+            transform: [{ rotate: `${rollAngle}deg` }],
+          }}
+        >
+          <Ionicons
+            name="ear"
+            size={60}
+            color="white"
+            style={{ transform: [{ rotateY: `180deg` }] }}
+          />
+        </View>
+      )}
+      {leftEar && (
+        <View
+          style={{
+            position: "absolute",
+            top: leftEar.y - 30,
+            left: leftEar.x,
+            height: 60,
+            width: 60,
+            transform: [{ rotate: `${rollAngle}deg` }],
+          }}
+        >
+          <Ionicons name="ear" size={60} color="white" />
+        </View>
+      )}
+      {nose && (
+        <View
+          style={{
+            position: "absolute",
+            top: nose.y - 30,
+            left: nose.x - 30,
+            height: 60,
+            width: 60,
+            transform: [{ rotate: `${rollAngle}deg` }],
+          }}
+        >
+          <Ionicons name="baseball" size={60} color="white" />
+        </View>
+      )}
+      {mouth && (
+        <View
+          style={{
+            position: "absolute",
+            top: mouth.y - 60,
+            left: mouth.x - 30,
+            height: 60,
+            width: 60,
+            transform: [{ rotate: `${rollAngle}deg` }],
+          }}
+        >
+          {smile >= 0.2 ? (
+            <Ionicons
+              name="ellipsis-horizontal-circle"
+              size={60}
+              color="white"
+            />
+          ) : (
+            <Ionicons name="ellipsis-horizontal" size={60} color="white" />
+          )}
         </View>
       )}
     </View>
@@ -85,7 +197,7 @@ function FaceDetection() {
 module.exports = FaceDetection;
 
 const styles = StyleSheet.create({
-  container: {
+  cameraContainer: {
     flex: 1,
     marginTop: 26,
     backgroundColor: "#fff",
