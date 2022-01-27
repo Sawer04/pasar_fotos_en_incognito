3909a449ca06111a6d6f31ca17d115d476cd51b1
anthony luque <kasuru@hotmail.com>
2022-01-27 18:56:41 -0500

    [ADD] sharePicture Screen,move that code

--- App.js
+++ App.js
@@ -1,119 +1,23 @@
-import React from "react";
-import {
-  Image,
-  StyleSheet,
-  Platform,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import * as ImagePicker from "expo-image-picker";
-import * as Sharing from "expo-sharing";
-import uploadToAnonymousFilesAsync from "anonymous-files";
-import FaceDetection from "./src/face_detection/FaceDetection";
+import react from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
-  const [selectedImage, setSelectedImage] = React.useState(null);
-
-  let openImagePickerAsync = async () => {
-    let permissionResult =
-      await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-    if (permissionResult.granted === false) {
-      alert("Necesitas acceso requerido a la camara");
-      return;
-    }
-
-    let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    if (pickerResult.cancelled === true) {
-      return;
-    }
-
-    if (Platform.OS === "web") {
-      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
-      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
-    } else {
-      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
-    }
-  };
-
-  let openShareDialogAsync = async () => {
-    if (!(await Sharing.isAvailableAsync())) {
-      alert(
-        `The image is available for sharing at: ${selectedImage.remoteUri}`
-      );
-      return;
-    }
-
-    await Sharing.shareAsync(selectedImage.localUri);
-  };
-
-  if (selectedImage !== null) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{ uri: selectedImage.localUri }}
-          style={styles.thumbnail}
-        />
-        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
-          <Text style={styles.buttonText}>comparteme aqui</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-
   return (
     <View style={styles.container}>
-      <Image
-        source={{
-          uri: "https://files.lafm.com.co/assets/public/2018-06/dia_de_la_lechona.jpg",
-        }}
-        style={styles.logo}
-      />
-
-      <Text style={styles.instructions}>
-        Presiona aqui para compartir la foto!
-      </Text>
-
-      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
-        <Text style={styles.buttonText}>oprimeme</Text>
-      </TouchableOpacity>
+      <Text style={styles.text}>Hola todos</Text>
     </View>
-    // <FaceDetection />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#98759846",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "purple",
   },
-
-  logo: {
-    width: 305,
-    height: 159,
-    marginBottom: 20,
-  },
-  instructions: {
-    color: "#000",
-    fontSize: 18,
-    marginHorizontal: 15,
-    marginBottom: 10,
-  },
-  button: {
-    backgroundColor: "green",
-    padding: 20,
-    borderRadius: 30,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: "#fff",
-  },
-  thumbnail: {
-    width: 500,
-    height: 500,
-    resizeMode: "contain",
+  text: {
+    color: "white",
+    fontSize: 50,
   },
 });
